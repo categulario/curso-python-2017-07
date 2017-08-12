d4ea9a5b202f33f48c81933542cf2b0abd47b081
Mario Contreras <macvirgo14@gmail.com>
2017-08-11 21:45:09 -0500

    Agregando las correcciones mencionadas por el teacher

--- homework/2017-07-31.py
+++ homework/2017-07-31.py
@@ -1,7 +1,7 @@
 def decorador(otrafuncion):
     def envoltura(x, y):
         print('antes')
-        valor = otrafuncion(x, y)
+        valor = otrafuncion((x+5), y)
         print('despues')
         return valor
 
@@ -12,6 +12,6 @@ def f(x, y):
     return x + y
 
 if __name__ == '__main__':
-    assert f(5, 0) == 5
-    assert f(5, -5) == 0
-    assert f(8, 2) == 10
+    assert f(5, 0) == 10
+    assert f(5, -5) == 5
+    assert f(8, 2) == 15
